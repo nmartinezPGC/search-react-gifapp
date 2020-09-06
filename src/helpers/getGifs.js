ea4cf173346aa99fe7486d38b3e259d22d142bf6
nmartinezsalgado <nmartinez.salgado@yahoo.com>
2020-09-06 15:10:44 -0600

    Primer commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=JLS35JB2ugAv6tbWm30AEB8HKqww6xbv`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    });
+
+    return gifs;
+};
