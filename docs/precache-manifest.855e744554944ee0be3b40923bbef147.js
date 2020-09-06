23e1b9718a10ba0113e640d68ddc7970e21a8b45
nmartinezsalgado <nmartinez.salgado@yahoo.com>
2020-09-06 15:31:47 -0600

    Modica md

---
+++ docs/precache-manifest.855e744554944ee0be3b40923bbef147.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2825bcc1fb38aafb9f8bd085fefbb725",
+    "url": "/index.html"
+  },
+  {
+    "revision": "247fdae8bfd009332f35",
+    "url": "/static/css/main.00331950.chunk.css"
+  },
+  {
+    "revision": "027ae5944804431f3db5",
+    "url": "/static/js/2.9f575afa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f575afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "247fdae8bfd009332f35",
+    "url": "/static/js/main.e2baa8fb.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
